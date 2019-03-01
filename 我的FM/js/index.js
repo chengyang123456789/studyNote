4ca81e4d26713b2abad0c40c684af9a74d702a90
chengyang123456 <741963962@qq.com>
2019-03-01 15:19:04 +0800

    fmjs提交

--- 我的FM/js/index.js
+++ 我的FM/js/index.js
@@ -7,41 +7,38 @@ var EventCenter = {
       $(document).trigger(type,data)
     }
   }
-  
-//   EventCenter.on('hello', function(e,data){
-//     console.log(data)
-//   })
-  
-//   EventCenter.fire('hello', '你好')
+
 
 var Footer = {
     init: function(){
         this.$footer = $('footer')
         this.$ul = this.$footer.find('ul')
         this.$box = this.$footer.find('.box')
-        this.$leftBtn = this.footer.find('.icon-left')
+        this.$leftBtn = this.$footer.find('.icon-left')
         this.$rightBtn = this.$footer.find('.icon-right')
-        this.isToend = false
+        this.isToEnd = false
         this.isToStart = true
         this.isAnimate = false
 
         this.blind()
         this.render()
     },
+
     blind:function(){
         var _this = this
         this.$rightBtn.on('click',function(){
-            if(_this.isAnimate) return
-
+            
             var itemWidth = _this.$box.find('li').outerWidth(true)
             var rowCount = Math.floor(_this.$box.width()/itemWidth)
             if(!_this.isToEnd){
-                _this.isAnimate = true
                 _this.$ul.animate({
-                    left: '-='+rowCOunt*itemWidth
+                    left: '-=' + rowCount * itemWidth + 'px'
                 },400,function(){
-                    _this.isAnimate = false
-                    _this.isToStart = false
+                    
+                    _this.isToStart = false 
+                    console.log('left ' + parseFloat(_this.$ul.css('left')))
+                    console.log('box-width '+ parseFloat(_this.$box.width()))
+                    console.log('ul-width '+ parseFloat(_this.$ul.css('width')))
                     if(parseFloat(_this.$box.width()) - parseFloat(_this.$ul.css('left')) >= parseFloat(_this.$ul.css('width'))){
                         _this.isToEnd = true
                     }
@@ -49,39 +46,35 @@ var Footer = {
             }
         })
 
-
         this.$leftBtn.on('click',function(){
-            if(_this.isAnimate) return
             var itemWidth = _this.$box.find('li').outerWidth(true)
-            var rowCount = Math.floor(_this.$box.width/itemWidth)
-            if(!_this.isToStart) {
-                _this.isAnimate = true
+            var rowCount = Math.floor(_this.$box.width()/itemWidth)
+            if(!_this.isToStart){
                 _this.$ul.animate({
-                    left:'+='+rowCount*itemWidth
+                    left: '+=' + rowCount * itemWidth
                 },400,function(){
                     _this.isToEnd = false
-                    _this.isAnimate = false
-                    if(parseFloat(_this.$ul.css('left')) >= 0){
+                    if(parseFloat(_this.$ul.css('left')) >=0){
                         _this.isToStart = true
                     }
                 })
             }
         })
 
-
         this.$footer.on('click','li',function(){
             $(this).addClass('active')
             .siblings().removeClass('active')
-
-            EventCenter.fire('select-albumn',{
+            EventCenter.fire('select-album',{
                 channelId:$(this).attr('data-channel-id'),
                 channelName:$(this).attr('data-channel-name')
             })
         })
     },
+
+
     render(){
         var _this = this
-        $.getJSON('//jirenguapi.applinzi.com/fm/getChannels.php')
+        $.getJSON('https://jirenguapi.applinzi.com/fm/getChannels.php')
         .done(function(ret){
             console.log(ret)
             _this.renderFooter(ret.channels)
@@ -101,10 +94,33 @@ var Footer = {
             cover_big: 'http://cloud.hunger-valley.com/17-10-24/1906806.jpg-big',
         })
         channels.forEach(function(channel){
-            html += '<li data-channel-id='+channel.channel_id+'data-channel-name='+channel.name+'>'
+            html += '<li data-channel-id='+channel.channel_id+'>'
+                    +'  <div class="cover" style="background-image:url('+channel.cover_small+')"></div>'
+                    + '  <h3>'+ channel.name+'</h3>'
+                    + '</li>'
                 
+        })
+        this.$ul.html(html)
+        this.setStyle()
+    },
+    setStyle:function(){
+        var count = this.$footer.find('li').length  //获得ｌｉ的个数
+        var width = this.$footer.find('li').outerWidth(true)　//获取宽度、包括外边距
+        this.$ul.css({
+            width:count * width + 'px'
         })
     }
 }
 
+var App = {
+    init:function(){
+        this.blind
+    },
+    bind:function(){
+        EventCenter.on('select-album',function(e,data){
+            console.log('select',data)
+        })
+    }
+}
 Footer.init()
+App.init()
